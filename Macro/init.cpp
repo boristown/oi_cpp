f58671c0b8d9a47924d9c5f6f118be634d60ff19
AI Era <47687653+boristown@users.noreply.github.com>
2022-02-11 17:34:46 +0800

    init

---
+++ Macro/init.cpp
@@ -0,0 +1,31 @@
+int arrayLenth(char *p)
+{
+    int len = 0;
+    while (*p)
+    {
+        p++;
+        len++;
+    }
+    return len;
+}
+
+int arrayLenth(int *p)
+{
+    int len = 0;
+    while (*p)
+    {
+        p++;
+        len++;
+    }
+    return len;
+}
+
+char* charArray(int n){
+    char *ans = (char *)malloc(n * sizeof(char));
+    return ans;
+}
+
+int* intArray(int n){
+    int *ans = (int *)malloc(n * sizeof(int));
+    return ans;
+}
